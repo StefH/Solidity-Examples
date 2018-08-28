4aea8f590eec032f6dec69d704010732fa35f2fd
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-08-28 08:29:10 +0200

    Include .sol file(s)

--- SmartContracts/Examples/UUIDProvider/Solidity/contracts/build/IUUIDProviderContractService.Generated.cs
+++ SmartContracts/Examples/UUIDProvider/Solidity/contracts/build/IUUIDProviderContractService.Generated.cs
@@ -1,17 +1,26 @@
-using System.Threading.Tasks;
+using System;
 using System.Numerics;
-using Nethereum.Hex.HexTypes;
+using System.Threading.Tasks;
 using Nethereum.Contracts;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
 
+// Created with 'Solidity to C# generator' by stef.heyenrath@mstack.nl (mStack B.V.)
+// Based on abi-code-gen (https://github.com/Nethereum/abi-code-gen)
 namespace ConsoleApp
 {
-   public interface IUUIDProviderContractService
-   {
+    public interface IUUIDProviderContractService
+    {
+        Task<TransactionReceipt> ExecuteTransactionAsync(Func<IUUIDProviderContractService, Task<string>> func, int timeoutInSeconds = 120);
+
+        Task<BlockWithTransactions> GetBlockWithTransactionsAsync(string blockhash);
+
         Function GetFunctionGenerateUUID4();
 
         Event GetUUIDEvent();
 
         Task<byte[]> GenerateUUID4CallAsync();
+        Task<byte[]> GenerateUUID4CallAsync(string addressFrom,  HexBigInteger gas = null, HexBigInteger valueAmount = null);
 
         Task<string> GenerateUUID4Async(string addressFrom,  HexBigInteger gas = null, HexBigInteger valueAmount = null);
 
